e186c37d95cde3e589f7333dd3520d2de865a654
jljimenezc <joseluis.jimenezcevallos@gmail.com>
2025-02-22 00:19:28 -0500

    Add styling improvements and new images.
     Improved the style of `header`, `form`, and `news-item` components for a more visually appealing look.
      - Adjusted typography for titles and forms to match the overall design.
      - Applied rounded borders, shadows, and gradients to enhance component aesthetics.
    - Added new images to the project.
      - Images added to the `/images` folder and properly referenced in the code.
      - Corrected the usage of relative paths for images in blog entry HTML code.
    - Updated CSS file to adjust image display within blog entries, ensuring they maintain a professional and attractive appearance.

--- Actividad5/src/app/app.component.ts
+++ Actividad5/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   newsList: INew[] = [
     {
       title: 'LDU de Quito gana el clásico del Ecuador',
-      imageUrl: 'https://example.com/ldu-quito-image.jpg',
+      imageUrl: 'images/Image1.jpg',
       text: 'LDU de Quito se impuso 3-1 a su eterno rival, Emelec, en una noche histórica...',
       imageFooterName: 'Foto de archivo: LDU de Quito',
       imageFooterDate: new Date('2025-02-21'),
@@ -23,7 +23,7 @@ export class AppComponent {
     },
     {
       title: 'El Nacional FC clasifica a la Copa Libertadores',
-      imageUrl: 'https://example.com/nacional-fc-image.jpg',
+      imageUrl: 'images/Image2.jpg',
       text: 'El Nacional FC consiguió su clasificación a la Copa Libertadores tras un empate clave...',
       imageFooterName: 'Foto de archivo: El Nacional FC',
       imageFooterDate: new Date('2025-02-20'),
